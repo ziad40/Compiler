c26ae6767fe1b9b86103e92de0ab642ef8573d21
ziad40 <zizoabuelkher@gmail.com>
2023-11-28 20:30:41 +0200

    update utilities function as after every operation we update ids

--- phase one/node.h
+++ phase one/node.h
@@ -4,26 +4,36 @@ class node
 {
     
 public:
-    vector<char> transation;
+    vector<char> transations;
     vector<node*> next_nodes;
     int id;
+    bool acceptance;
     node(vector<char> transation){
         for(char x : transation){
-            this->transation.push_back(x);
-        }  
+            this->transations.push_back(x);
+        }
+        acceptance = false;  
     }
 
     node(vector<char> transation, vector<node*>& next_nodes){
         for(char x : transation){
-            this->transation.push_back(x);
+            this->transations.push_back(x);
         } 
         this->next_nodes = next_nodes;
+        acceptance = false;  
     }
 
-    node(){}
+    node(){
+        acceptance = false;  
+    }
+
+    node(int ID){
+        id = ID;
+        acceptance = false;  
+    }
 
     void add_next_node(node& new_node){
-        node n = node(new_node.transation, new_node.next_nodes);
+        node n = node(new_node.transations, new_node.next_nodes);
         next_nodes.push_back(&n);
     }
 };
