fe8c67bfb216f16f79754260c1702c3761f6ef10
AbdelRahman Adel AbdelFatttah <56457765+ad3ldev@users.noreply.github.com>
2023-12-08 01:15:54 +0200

    main

--- parser.cpp
+++ parser.cpp
@@ -93,13 +93,3 @@ void Parser::parse(vector<Node *> dfa, string path){
         }
     }
 }
-
-int main(int argc, char* argv[])
-{
-    string relative_path = "../input_example.txt";
-    vector<Node*> dfa = lexical_analysis(relative_path);
-    string file = "../Main.java";
-    Parser parser;
-    parser.parse(dfa, file);
-    return 0;
-}
