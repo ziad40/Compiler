e476ee7b12cb7c5a770c950a802a24b3f410def6
AliELSharawy <hsnalshrawyly56@gmail.com>
2023-12-08 16:41:50 +0200

    GO hesham

--- Main.java
+++ Main.java
@@ -1,4 +1,6 @@
-int sum, count, pass, mnt;
-while (pass != 10) {
-    pass = pass + 1;
-}
+int n = 3
+float f = 56.7;
+float f2 = 5.67E1
+x x,x 5 n
+if (f >50) { f = f2 / 2}
+else { f = f2 * 2}
--- lexical_analyzer.cpp
+++ lexical_analyzer.cpp
@@ -1,6 +1,6 @@
 #include "lexical_analyzer.h"
 
-int Node::counter = 1;
+//int Node::counter = 1;
 vector<Node*> lexical_analysis(string relative_path, read_input& r){
     r.read_lines(relative_path);
     NFA* s = r.routing_nfa;
@@ -11,20 +11,15 @@ vector<Node*> lexical_analysis(string relative_path, read_input& r){
     cout << "Intital Node ID = " << dfa.DFA_start_node.id << endl;
     cout << "Number of States = " << dfa.DFA_node_map.size() << endl;
     dfa.print_dfa();
-    cout << "-------------------------------------------------------------------------------" << endl;
-    vector<Node*> minimized_dfa = dfa.minimize();
-    cout << "--------------------------------------------------------------------------------" << endl;
-    dfa.print_dfa();
-    cout << "-----------------------------------------------------------------------------------" << endl;
-    for(Node* node : minimized_dfa){
-        cout << node->id << "   " << node->acceptance << "     " << endl;
-        for(auto &entry : node->transitions){
-            cout << entry.first << "->";
-            for(Node* next : entry.second){
-                cout << next->id << "   ";
-            }
-        }
-        cout<<endl;
-    }
-    return minimized_dfa;
+//    cout << "-------------------------------------------------------------------------------" << endl;
+//    vector<Node*> minimized_dfa = dfa.minimize();
+//    cout << "--------------------------------------------------------------------------------" << endl;
+//    dfa.print_dfa();
+//    cout << "-----------------------------------------------------------------------------------" << endl;
+
+
+    vector<Node*> nn;
+    for(auto &entry : dfa.DFA_node_map)
+        nn.push_back(entry.second);
+    return nn;
 }
