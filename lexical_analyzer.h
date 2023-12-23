d9fc20d9d137a154630c8981e4610f4f12a3a1db
ziad40 <zizoabuelkher@gmail.com>
2023-12-23 18:44:33 +0200

    rename files only

--- lexical_analyzer.h
+++ lexical_analyzer.h
@@ -2,7 +2,7 @@
 #ifndef LEXICAL_ANALYZER_H
 #define LEXICAL_ANALYZER_H
 
-#include "read_input.cpp"
+#include "lexical_rules.cpp"
 #include "DFA.cpp"
 vector<Node*> lexical_analysis(string relative_path, read_input& r);
 #endif //LEXICAL_ANALYZER_H
