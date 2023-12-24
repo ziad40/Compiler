c04d1184262d8e3870b004dae16e33405af5bdc1
ziad40 <zizoabuelkher@gmail.com>
2023-12-25 00:45:38 +0200

    batch 2 with pointers

--- production.h
+++ production.h
@@ -7,20 +7,31 @@
 
 #include <string>
 #include <vector>
+#include <set>
 
 using namespace std;
 class rule{
 public:
     string name;
     bool terminal;
-    vector<rule> first;
-    vector<rule> follow;
-    vector<vector<rule>> productions;
-    rule(string name, bool terminal){
+    bool has_epsilon_first ;
+    bool epsilon;
+    bool first_found ;
+    set<rule*> first;
+    set<rule*> follow;
+    vector<vector<rule*>> productions;
+    map<rule*, vector<rule*>> first_to_expression;
+    rule(string& name, bool terminal){
         this->terminal = terminal;
         this->name = name;
+        this->epsilon = false;
     }
-    void add_productions(const vector<rule> p){
+    rule(){
+        this->epsilon = true;
+        this->terminal = true;
+        this->name = "epsilon";
+    }
+    void add_productions(const vector<rule*>& p){
         this->productions.push_back(p);
     }
     void get_first(){
