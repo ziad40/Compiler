0bd79d9560936bd61d0a99e2de128b2dfea476e4
Ahmed Hesham <ahmedhesham122000@gmail.com>
2023-12-25 03:41:10 +0200

    finish follow and first
    now we can get first and follow for any of the input

--- production.h
+++ production.h
@@ -16,9 +16,9 @@ class rule{
 public:
     string name;
     bool terminal;
-    bool has_epsilon_first;
-    bool epsilon;
-    bool first_found;
+    bool has_epsilon_first = false; //if current rule has epsilon
+    bool epsilon;           //if the current is epsilon --> we will try to delete it later
+    bool first_found = false;       //if the first is found for the current token
     set<rule*> first;
     set<rule*> follow;
     vector<vector<rule*>> productions;
@@ -35,38 +35,46 @@ public:
     void get_first(){
 
         if(!(this -> first_found)){
-            //terminals has them selves as first 
+            //terminals has themselves as first
             if(this -> terminal){
                 this -> first.insert(this);
             }
             else{
-                //go through all productes and if they are non terminals get their first otherwise the terminal is the first
+                //go through all productes and if they are non-terminals get their first otherwise the terminal is the first
                 for(auto p: this->productions){
                     //recursivly calculate the first of all needed productions 
                     if(p[0]-> first.size() == 0){
                         p[0] -> get_first();
                     }
 
-                    //non-terminal 
+                    //non-terminal just add them
                     if(p[0]->terminal){
-                        set<rule*>::iterator it = p[0]-> first.begin();
+                        auto it = p[0]-> first.begin();
                         this->first.insert(*it);
                         this->first_to_expression[*it] = p;
                     }
 
                     if(p[0]->epsilon)
                         this-> has_epsilon_first = true;
-                        int i = 0;
+
+                    int i = 0;
                     if(!(p[0]->terminal) && i < p.size()){
                         for(rule* r: p[0]->first){
                             this-> first.insert(r);
                             this-> first_to_expression[r]= p;
                         }
-                        while(p[i]->has_epsilon_first)
-                        i++;
-                        for(rule* r: p[i]->first) {
-                            this->first.insert(r);
-                            this->first_to_expression[r] = p;
+                        while(p[i]->has_epsilon_first) {
+                            i++;
+                            for (rule *r: p[i]->first) {
+                                this->first.insert(r);
+                                this->first_to_expression[r] = p;
+                            }
+                            if(i == p.size()-1){
+                                rule temp = rule("epsilon", true);
+                                temp.epsilon = true;
+                                this->first.insert(&temp);
+                                this-> has_epsilon_first = true;
+                            }
                         }
                     }
                 }
@@ -75,13 +83,70 @@ public:
         }
     }
 
+    void get_follow(const vector<rule*>& full_productions){
+        if(!(this->terminal)) {
+            for (auto p: full_productions) {
+                for (auto v: p->productions) {
+                    for (int i = 0; i < v.size(); i++) {
+                        if (v[i] == this){
+                            int index = i;
+                            bool follow_rule = true;
+                            if(i != v.size()-1) {
+                                if(v[i+1]->terminal){
+                                    this->follow.insert(v[i+1]);
+                                }
+                                else{
+                                    for(auto j : v[i+1]->first) {
+                                        if (!(j->epsilon)) {
+                                            this->follow.insert(j);
+                                        }
+                                    }
+                                }
+                            }else{
+                                if(p->follow.size() == 0)
+                                    p->get_follow(full_productions);
 
+                                this->follow.insert(p->follow.begin(), p->follow.end());
+                            }
 
-   
+                            for(int j = index+1; j < v.size(); j++){
+                                follow_rule &= v[j]->has_epsilon_first;
+                            }
 
+                            if(follow_rule){
+                                this->follow.insert(p->follow.begin(), p->follow.end());
+                            }
+                        }
+                    }
 
-    void get_follow(){
+                }
+            }
+        }
+/*
+        if(!(this-> terminal)){
+            for(auto p: productions){
+                for (int i = 0; i < p.size(); i++){
+                    if(p[i] == this){
+                        if(i != p.size()-1) {
+                            //if the following is terminal then it's the following
+                            if (p[i + 1]->terminal) {
+                                this->follow.insert(p[i + 1]);
+                            //if the following is not terminal then all then its first is following for the current token
+                            } else {
+                                this-> follow.insert(p[i+1]->first.begin(),p[i+1]->first.end());
+                            }
+                        }
+                    }
+                }
+            }
 
-    }
+            for(auto p: this -> productions){
+                if(!(p[p.size()-1]->terminal)){
+                    p[p.size()-1] ->follow.insert(this->follow.begin(), this->follow.end());
+                }
+            }
+
+        }
+    */}
 };
 #endif //PROJECT_PRODUCTION_H
--- production_test.cpp
+++ production_test.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 #include "production.h"
 
-int main(){
-
+void example1(){
     rule r1 = rule("S", false);
     r1.epsilon = false;
-    
+    r1.follow.insert(new rule("$", false));
+
     rule r2 = rule("i", true);
     r2.epsilon = false;
 
@@ -74,6 +74,12 @@ int main(){
     r5.get_first();
     r3.get_first();
 
+    vector<rule*> rules = {&r1, &r2, &r3, &r4, &r5, &r6, &r7, &r8, &r9};
+
+    r1.get_follow(rules);
+    r5.get_follow(rules);
+    r3.get_follow(rules);
+
 
     for (auto p: r1.first)
         cout << p->name << " ";
@@ -86,5 +92,164 @@ int main(){
     for (auto p: r3.first)
         cout << p->name << " ";
     cout << endl;
+
+}
+
+void example2(){
+    string name = "eps";
+    rule* eps = new rule(name,true);
+    eps->epsilon = true;
+
+    name = "$";
+    rule* dollar = new rule(name, true);
+
+    name = "+";
+    rule* plus = new rule(name, true);
+
+    name = "*";
+    rule* multiply = new rule(name, true);
+
+    name = "(";
+    rule* open = new rule(name, true);
+
+    name = ")";
+    rule* close = new rule(name, true);
+
+    name = "id";
+    rule* id = new rule(name, true);
+
+
+
+    name = "E";
+    rule* E = new rule(name, false);
+
+    name = "E'";
+    rule* E_dash = new rule(name, false);
+
+    name = "T";
+    rule* T = new rule(name, false);
+
+    name = "T'";
+    rule* T_dash = new rule(name, false);
+
+    name = "F";
+    rule* F = new rule(name, false);
+
+    // productions
+    vector<rule*> prodE1;
+    prodE1.push_back(T);
+    prodE1.push_back(E_dash);
+
+    E->productions.push_back(prodE1);
+
+    vector<rule*> prodE_dash1;
+    prodE_dash1.push_back(plus);
+    prodE_dash1.push_back(T);
+    prodE_dash1.push_back(E_dash);
+
+    vector<rule*> prodE_dash2;
+    prodE_dash2.push_back(eps);
+
+    E_dash->productions.push_back(prodE_dash1);
+    E_dash->productions.push_back(prodE_dash2);
+
+
+    vector<rule*> prodT1;
+    prodT1.push_back(F);
+    prodT1.push_back(T_dash);
+
+    T->productions.push_back(prodT1);
+
+    vector<rule*> prodT_dash1;
+    prodT_dash1.push_back(multiply);
+    prodT_dash1.push_back(F);
+    prodT_dash1.push_back(T_dash);
+
+    vector<rule*> prodT_dash2;
+    prodT_dash2.push_back(eps);
+
+    T_dash->productions.push_back(prodT_dash1);
+    T_dash->productions.push_back(prodT_dash2);
+
+
+    vector<rule*> prodF1;
+    prodF1.push_back(open);
+    prodF1.push_back(E);
+    prodF1.push_back(close);
+
+    vector<rule*> prodF2;
+    prodF2.push_back(id);
+
+    F->productions.push_back(prodF1);
+    F->productions.push_back(prodF2);
+
+    E->get_first();
+    E_dash -> get_first();
+    T -> get_first();
+    T_dash -> get_first();
+    F -> get_first();
+
+    vector<rule*> rules = {E, E_dash, T, T_dash, F, eps, dollar, multiply, plus, open, close, id};
+
+    E -> get_follow(rules);
+    E_dash -> get_follow(rules);
+    T-> get_follow(rules);
+    T_dash-> get_follow(rules);
+    F-> get_follow(rules);
+
+    cout << "E:\nfirst:" << endl;
+    for (auto p: E->first)
+        cout << p->name << " ";
+    cout << endl;
+
+    cout << "follow: "<<endl;
+    for (auto p: E->follow)
+        cout << p->name << " ";
+    cout << endl;
+
+
+    cout << "E`:\nfirst:" << endl;
+    for (auto p: E_dash->first)
+        cout << p->name << " ";
+    cout << endl;
+
+    cout << "follow: "<<endl;
+    for (auto p: E_dash->follow)
+        cout << p->name << " ";
+    cout << endl;
+
+    cout << "T:\nfirst:" << endl;
+    for (auto p: T->first)
+        cout << p->name << " ";
+    cout << endl;
+
+    cout << "follow: "<<endl;
+    for (auto p: T->follow)
+        cout << p->name << " ";
+    cout << endl;
+
+    cout << "T`:\nfirst:" << endl;
+    for (auto p: T_dash->first)
+        cout << p->name << " ";
+    cout << endl;
+
+    cout << "follow: "<<endl;
+    for (auto p: T_dash->follow)
+        cout << p->name << " ";
+    cout << endl;
+
+    cout << "F:\nfirst:" << endl;
+    for (auto p: F->first)
+        cout << p->name << " ";
+    cout << endl;
+
+    cout << "follow: "<<endl;
+    for (auto p: F->follow)
+        cout << p->name << " ";
+    cout << endl;
+}
+
+int main() {
+    example2();
     return 0;
 }
